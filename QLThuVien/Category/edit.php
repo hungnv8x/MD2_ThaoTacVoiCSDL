94df26ed9e44e3353f1e8a9bc32cbac5b12502da
Hưng Nguyễn Văn <hungnvhb1988@gmail.com>
2022-02-10 11:05:04 +0700

    ThaotacVoiCSDL

---
+++ QLThuVien/Category/edit.php
@@ -0,0 +1,37 @@
+<?php
+require_once '../database/databaseconnect.php';
+if (isset($conn)) {
+    $stmt = $conn->prepare('select * from category where id ='.$_REQUEST['id']);
+}
+$stmt ->setFetchMode(PDO::FETCH_OBJ);
+$stmt->execute();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="" method="post">
+    <?php while ($row = $stmt->fetch()) { ?>
+    <input type="text" name="id" disabled value="<?php echo $row->id ?>">
+    <input type="text" name="name" value="<?php echo $row->name?>">
+    <?php } ?>
+    <button>Update</button>
+</form>
+</body>
+</html>
+<?php
+if ($_SERVER['REQUEST_METHOD']=="POST"){
+    if (isset($conn)) {
+        $sql = 'update category set name ='.$_REQUEST['name'].'where id ='.$_REQUEST['id'];
+        $conn->exec($sql);
+        header('location:displaycategory.php');
+        echo "hello";
+    }
+}
+?>
